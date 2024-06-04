67e59bb634f3cb12386b783dda184e151a7dda4b
thi1604 <thibeo1604@gmail.com>
2024-06-04 14:50:30 +0700

    Tao Pug va chinh lai thu muc views

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('views', './views');
+app.set('views engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render("client/pages/home/index.pug");
+});
+
+app.get('/product', (req, res) => {
+    res.render("client/pages/product/index.pug");
+});
+
+app.listen(port, () => {
+    console.log(`Dang chay cong ${port}`);
+});
+
+
 
