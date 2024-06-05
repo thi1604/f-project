04f4fffeedbf49cc57372c75faa74a00d835d75a
thi1604 <thibeo1604@gmail.com>
2024-06-05 22:10:28 +0700

    Cau hinh lai file pug

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,7 +1,11 @@
 module.exports.index = (req, res)=>{
-    res.render("client/pages/product/index.pug");
+    res.render("client/pages/product/index.pug", {
+        pageTitle: "Trang danh sach san pham"
+    });
 };
 
 module.exports.creat = (req, res)=>{
-    res.render("client/pages/product/creat.pug");
+    res.render("client/pages/product/creat.pug", {
+        pageTitle: "Trang tao moi sp"
+    });
 };
