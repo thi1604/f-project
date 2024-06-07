7e842300e11dceb91b2421c62c6dcf7a90970737
thi1604 <thibeo1604@gmail.com>
2024-06-07 15:00:00 +0700

    Sua layout trang san pham ben admin, do data ra giao dien

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,4 +1,11 @@
-module.exports.index = (req, res) => {
-  res.render("admin/pages/products/index.pug");
+const product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+  const Product = await product.find({
+    deleted: false
+  });
+  res.render("admin/pages/products/index.pug", {
+    listProducts: Product 
+  });
 }
 
