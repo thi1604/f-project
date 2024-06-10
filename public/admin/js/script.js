834b8e6c480f22ff50b3b309c5e014f90268a475
thi1604 <thibeo1604@gmail.com>
2024-06-10 22:58:38 +0700

    Xu li data ben bo loc BE, do data ra giao dien ben Admin

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,25 @@
-console.log("Thi Beo");
+
+const listProducts = document.querySelectorAll("[button-status]");
+
+let url = new URL(window.location.href);
+
+listProducts.forEach((item)=>{
+  item.addEventListener("click", ()=>{
+    const status = item.getAttribute("button-status");
+    if(status != ""){
+      url.searchParams.set("status", status);
+    }
+    else{
+      url.searchParams.delete("status");
+    }
+    window.location.href = url;
+  });
+});
+
+
+const currentStatus = url.searchParams.get("status") || ""; 
+// Khi lay thuoc tinh tu dinh nghia, ta phai co dau []
+const currentButton = document.querySelector(`[button-status = "${currentStatus}"]`);
+currentButton.classList.add("active");
+
+
