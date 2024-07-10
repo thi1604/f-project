e170d96b84366efcdceb0c5d86ab414b283cd7e3
thi1604 <thibeo1604@gmail.com>
2024-07-10 17:31:50 +0700

    Tao phan chinh sua danh muc, (chu y xu li mang Tree ben BE roi moi gui mang sang FE

--- controller/admin/category.controller.js
+++ controller/admin/category.controller.js
@@ -1,5 +1,7 @@
 const ProductCategory = require("../../models/product-category.model");
 const selectTreeHelper = require("../../helper/select-tree.helper");
+const prefix = require("../../config/system");
+
 module.exports.index = async (req, res) => {
   const record = await ProductCategory.find({
     deleted: false
@@ -35,4 +37,42 @@ module.exports.createPost = async (req, res) => {
   req.flash("success", "Thêm danh mục thành công !");
   await newCategory.save(); // Phai co tu await(Doi luu vao database, ko co se chua kip luu)
   res.redirect("/admin/products-category");
+}
+
+module.exports.edit = async (req, res) => {
+  try{
+    const id = req.params.id;
+    const record = await ProductCategory.findOne({
+      _id: id
+    });
+    const records = await ProductCategory.find({
+      deleted: false
+    });
+
+    const listRecord = selectTreeHelper(records);
+    if(record){
+      res.render(`${prefix}/pages/products-category/edit.pug`,{
+        pageTitle: "Trang chỉnh sửa danh mục",
+        product: record,
+        listRecord: listRecord
+      });
+    }
+    else{
+      res.redirect(`/${prefix}/products-category`);
+    }
+
+  }
+  catch(error){
+    res.redirect(`/${prefix}/products-category`);
+  }
+}
+
+module.exports.editPatch = async (req, res)=> {
+  const id = req.params.id;
+  await ProductCategory.updateOne(
+  {
+    _id : id
+  }, req.body);
+  
+  res.redirect('back');
 }
