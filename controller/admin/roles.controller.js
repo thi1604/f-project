18cb684c0bcc66b6e59489ceab0bd91e36c6b25f
thi1604 <thibeo1604@gmail.com>
2024-07-11 16:27:39 +0700

    Tao phan chinh sua nhom quyen

--- controller/admin/roles.controller.js
+++ controller/admin/roles.controller.js
@@ -1,4 +1,5 @@
 const roles = require("../../models/roles.model");
+const prefix = require("../../config/system");
 
 
 module.exports.index = async (req, res)=>{
@@ -6,20 +7,53 @@ module.exports.index = async (req, res)=>{
     deleted: false
   });
 
-  res.render("admin/pages/roles/index.pug", {
+  res.render(`${prefix}/pages/roles/index.pug`, {
     pageTitle: "Trang nhóm quyền",
     records: records
   });
 };
 
 module.exports.create = (req, res)=>{
-  res.render("admin/pages/roles/create.pug", {
+  res.render(`${prefix}/pages/roles/create.pug`, {
     pageTitle: "Trang nhóm quyền",
   });
 };
 
 module.exports.createPost = async (req, res)=>{
+  req.flash("success", "Tạo mới thành công!");
   const newRecord = new roles(req.body);
   await newRecord.save();
-  res.redirect("/admin/roles");
+  res.redirect(`/${prefix}/roles`);
+};
+
+module.exports.edit = async (req, res)=>{
+  const id = req.params.id;
+  try{
+    const record = await roles.findOne({
+      _id: id
+    });
+    if(record){
+      res.render(`${prefix}/pages/roles/edit.pug`, {
+        pageTitle: "Chỉnh sửa nhóm quyền",
+        record: record
+      });
+    }
+    else{
+      req.flash("error", "Lỗi!");
+      res.redirect(`/${prefix}/roles`);
+    }
+  }
+  catch(error){
+    req.flash("error", "Lỗi!");
+    res.redirect(`/${prefix}/roles`);
+  }
+};
+
+module.exports.editPatch = async (req, res)=>{
+  const id = req.params.id;
+  await roles.updateOne({
+    _id: id
+  }, req.body);
+  req.flash("success", "Đã cập nhật!");
+  res.redirect('back');
 };
--- routes/admin/roles.router.js
+++ routes/admin/roles.router.js
@@ -5,7 +5,12 @@ const controller = require("../../controller/admin/roles.controller");
 router.get("/", controller.index);
 
 router.get("/create", controller.create);
+
 router.post("/create", controller.createPost);
+
+router.get("/edit/:id", controller.edit);
+
+router.patch("/edit/:id", controller.editPatch);
 
 
 module.exports = router;
