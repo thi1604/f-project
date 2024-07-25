ff1baefd1080702c58e2f96d42b3faee04d17f4c
thi1604 <thibeo1604@gmail.com>
2024-07-25 12:02:07 +0700

    Hien thi san pham noi bat va moi nhat(toi da 6 san pham) ben trang client

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -1,5 +1,43 @@
-module.exports.index = ((req, res)=>{
+const productModel = require("../../models/product.model");
+
+
+module.exports.index = async (req, res)=>{
+
+    // feautedProducts
+    const feautedProducts = await productModel
+    .find({
+        feauted: "1",
+        deleted : false,
+        status : "active"
+    })
+    .sort({ possition :"desc"})
+    .limit(6)
+    .select("-description");
+
+    for (const item of feautedProducts) {
+        item.priceNew = ((1 - item.discountPercentage/100)* item.price).toFixed(0);
+    }
+    // EndfeautedProducts
+
+
+    // listNewProducts
+    const newProducts = await productModel
+    .find({
+        deleted : false,
+        status : "active"
+    })
+    .sort({ position :"desc"})
+    .limit(6)
+    .select("-description");
+
+    for (const item of newProducts) {
+        item.priceNew = ((1 - item.discountPercentage/100)* item.price).toFixed(0);
+    }
+    // End listNewProducts
+
     res.render("client/pages/home/index.pug", {
-        pageTitle: "Trang chu"
+        pageTitle: "Trang chu",
+        feautedProducts : feautedProducts,
+        newProducts: newProducts
     });
-})
+};
